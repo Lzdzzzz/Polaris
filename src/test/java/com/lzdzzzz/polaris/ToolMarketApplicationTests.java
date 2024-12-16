dfbf5b8d07ffb071ba981868aa76013b562cd1e3
lzd <luozhid@163.com>
2024-12-16 15:51:09 +0800

    Project initialization

---
+++ src/test/java/com/lzdzzzz/polaris/ToolMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzdzzzz.polaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
